661bff45ac1dc6f4701fb0966d40cea91b219842
kodakjerec0425 <kodakjerec@gmail.com>
2020-11-10 16:33:25 +0800

    V1

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace AforgeCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.880497F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 93.1195F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(624, 469);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(618, 431);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(618, 26);
+            this.panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button2.Location = new System.Drawing.Point(196, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 26);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Location = new System.Drawing.Point(121, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipTitle = "動態偵測!";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.notifyIcon1_BalloonTipClicked);
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(124, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(123, 22);
+            this.toolStripMenuItem1.Text = "Leave(&X)";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 469);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision.Motion;
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AforgeCam
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection VideoCaptureDevices;
+        private VideoCaptureDevice FinalVideo;
+        private MotionDetector detector;
+        private Rectangle[] zonesFrame, zonesPaint;
+
+        // Importante        //
+        // Installare AForge //
+
+        public Form1() // init
+        {
+            InitializeComponent();
+            {
+                VideoCaptureDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                foreach (FilterInfo VideoCaptureDevice in VideoCaptureDevices)
+                {
+                    comboBox1.Items.Add(VideoCaptureDevice.Name);
+                }
+                comboBox1.SelectedIndex = 0;
+
+                // resize image
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                // create motion detector
+                detector = new MotionDetector(
+                    new SimpleBackgroundModelingDetector(),
+                    new MotionAreaHighlighting());
+
+            }
+        }
+
+        /// <summary>
+        /// 開起鏡頭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FinalVideo = new VideoCaptureDevice(VideoCaptureDevices[comboBox1.SelectedIndex].MonikerString);
+            FinalVideo.VideoResolution = FinalVideo.VideoCapabilities[0];
+            FinalVideo.NewFrame += new NewFrameEventHandler(FinalVideo_NewFrame);
+            FinalVideo.Start();
+            setDetectZone();
+        }
+
+        /// <summary>
+        /// 關閉鏡頭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
+        void FinalVideo_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Image oldImage = pictureBox1.Image;
+            Bitmap video = (Bitmap)eventArgs.Frame.Clone();
+
+            if (detector.ProcessFrame(video) > 0.05) {
+                notifyIcon1.BalloonTipText = DateTime.Now.ToString("HH:mm:ss.fff");
+                notifyIcon1.ShowBalloonTip(1000);
+            }
+
+            pictureBox1.Image = video;
+
+            if (oldImage != null) {
+                oldImage.Dispose();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (FinalVideo != null)
+            {
+                FinalVideo.SignalToStop();
+                FinalVideo.WaitForStop();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (FinalVideo != null)
+            {
+                FinalVideo.SignalToStop();
+                FinalVideo.WaitForStop();
+            }
+        }
+
+        /// <summary>
+        /// 改變視窗比例, 也要改變paintSize
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
+            else
+            {
+                setDetectZone();
+            }
+        }
+
+        /// <summary>
+        /// new detectZone
+        /// </summary>
+        private void setDetectZone() {
+            if (FinalVideo == null) {
+                return;
+            }
+
+            Rectangle rectangle1,rectangle2,rectangle3;
+
+            if (zonesFrame == null)
+            {
+                rectangle1 = new Rectangle(0, 0, FinalVideo.VideoResolution.FrameSize.Width, 280);
+                rectangle2 = new Rectangle(0, 280, 350, 440);
+                rectangle3 = new Rectangle(FinalVideo.VideoResolution.FrameSize.Width - 350, 280, 350, 440);
+                zonesFrame = new Rectangle[] { rectangle1, rectangle2, rectangle3 };
+
+                detector.MotionZones = zonesFrame;
+            }
+            else {
+                rectangle1 = zonesFrame[0];
+                rectangle2 = zonesFrame[1];
+                rectangle3 = zonesFrame[2];
+            }
+
+
+
+            float percentWidth = (float)pictureBox1.Width / (float)FinalVideo.VideoResolution.FrameSize.Width;
+            float percentHeight = (float)pictureBox1.Height / (float)FinalVideo.VideoResolution.FrameSize.Height;
+
+            Rectangle rectanglePaint1 = new Rectangle(0, 0, FinalVideo.VideoResolution.FrameSize.Width, (int)(rectangle1.Height * percentHeight));
+            Rectangle rectanglePaint2 = new Rectangle(0, (int)(rectangle2.Y * percentHeight), (int)(rectangle2.Width * percentWidth), (int)(rectangle2.Height * percentHeight));
+            Rectangle rectanglePaint3 = new Rectangle((int)(rectangle3.X * percentWidth), (int)(rectangle3.Y * percentHeight), (int)(rectangle3.Width * percentWidth), (int)(rectangle3.Height * percentHeight));
+
+            zonesPaint = new Rectangle[] { rectanglePaint1, rectanglePaint2, rectanglePaint3 };
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                //如果目前是縮小狀態，才要回覆成一般大小的視窗
+                Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                //如果目前是縮小狀態，才要回覆成一般大小的視窗
+                Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        /// <summary>
+        /// 繪製方框
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (zonesPaint == null)
+            {
+                return;
+            }
+
+            using (Pen pen = new Pen(Color.Red)) {
+                for (int i = 0; i < zonesPaint.Length; i++) {
+                    e.Graphics.DrawRectangle(pen, zonesPaint[i]);
+                }
+            }
+        }
+    }
+}
